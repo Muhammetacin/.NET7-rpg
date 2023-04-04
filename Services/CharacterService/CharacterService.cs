9c337eef9842a8775ca90ff0e5b84eab89236cfd
Muhammet Cin <muhammetacin@gmail.com>
2023-04-04 20:32:09 +0200

    Add AutoMapper and implement

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -15,19 +15,27 @@ namespace Services.CharacterService
                 Name = "Sam"
             }
         };
+        private readonly IMapper _mapper;
+
+        public CharacterService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
 
         public async Task<ServiceResponse<List<GetCharacterResponseDTO>>> AddNewCharacter(AddCharacterRequestDTO newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterResponseDTO>>();
-            characters.Add(newCharacter);
-            serviceResponse.Data = characters;
+            var character = _mapper.Map<Character>(newCharacter);
+            character.Id = characters.Max(c => c.Id) + 1;
+            characters.Add(character);
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterResponseDTO>(c)).ToList();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<List<GetCharacterResponseDTO>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterResponseDTO>>();
-            serviceResponse.Data = characters;
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterResponseDTO>(c)).ToList();
             return serviceResponse;
         }
 
@@ -35,7 +43,7 @@ namespace Services.CharacterService
         {
             var serviceResponse = new ServiceResponse<GetCharacterResponseDTO>();
             var character = characters.FirstOrDefault(c => c.Id == id);
-            serviceResponse.Data = character;
+            serviceResponse.Data = _mapper.Map<GetCharacterResponseDTO>(character);
             
             return serviceResponse;
         }
